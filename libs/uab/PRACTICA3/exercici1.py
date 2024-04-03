949df2a715698f2a1278ed0f9c399b7f0666a375
yeray142 <yeraycorderocarrasco@gmail.com>
2024-04-03 09:35:17 +0200

    Block matching
    Exercises 1 and 2

---
+++ libs/uab/PRACTICA3/exercici1.py
@@ -0,0 +1,74 @@
+from metrikz import *
+import numpy as np
+
+
+def calcular_sad(bloque1, bloque2):
+    """Calcula la Suma de Diferencias Absolutas (SAD) entre dos bloques."""
+    return np.sum(np.abs(bloque1 - bloque2))
+
+
+def block_matching(frame_actual, frame_anterior, tamaño_bloque=8, restriccion=False, tamaño_ventana=24):
+    alto, ancho = frame_actual.shape
+    vectores_movimiento = []
+    errores_prediccion = []
+    mse_total = 0
+
+    for y in range(0, alto, tamaño_bloque):
+        for x in range(0, ancho, tamaño_bloque):
+            bloque_actual = frame_actual[y:y + tamaño_bloque, x:x + tamaño_bloque]
+            sad_min = np.inf
+            mejor_movimiento = (0, 0)
+            mejor_bloque = None
+
+            # Determinar los límites de la región de búsqueda
+            y_min = max(0, y - tamaño_ventana // 2) if restriccion else 0
+            y_max = min(alto, y + tamaño_ventana // 2 + tamaño_bloque) if restriccion else alto
+            x_min = max(0, x - tamaño_ventana // 2) if restriccion else 0
+            x_max = min(ancho, x + tamaño_ventana // 2 + tamaño_bloque) if restriccion else ancho
+
+            for y_ref in range(y_min, y_max - tamaño_bloque + 1):
+                for x_ref in range(x_min, x_max - tamaño_bloque + 1):
+                    bloque_ref = frame_anterior[y_ref:y_ref + tamaño_bloque, x_ref:x_ref + tamaño_bloque]
+                    sad_actual = calcular_sad(bloque_actual, bloque_ref)
+
+                    if sad_actual < sad_min:
+                        sad_min = sad_actual
+                        mejor_movimiento = (x_ref - x, y_ref - y)
+                        mejor_bloque = bloque_ref
+
+            vectores_movimiento.append(mejor_movimiento)
+            errores_prediccion.append(sad_min)
+            mse_total += mse(bloque_actual, mejor_bloque)
+
+    mse_promedio = mse_total / len(vectores_movimiento)
+    return vectores_movimiento, errores_prediccion, mse_promedio
+
+
+from PIL import Image
+import numpy as np
+
+# Cargar las imágenes y convertirlas a escala de grises
+frame_anterior = Image.open('frame1_1.png').convert('L')
+frame_actual = Image.open('frame1_2.png').convert('L')
+
+# Convertir las imágenes a arrays de NumPy
+frame_anterior_np = np.array(frame_anterior)
+frame_actual_np = np.array(frame_actual)
+
+# Imprimir las dimensiones para confirmar
+print("Frame Anterior:", frame_anterior_np.shape)
+print("Frame Actual:", frame_actual_np.shape)
+
+
+# Definir el tamaño del bloque
+tamaño_bloque = 8
+
+# Aplicar el algoritmo de Block Matching
+vectores_movimiento, errores_prediccion, mse_promedio = block_matching(
+    frame_actual_np, frame_anterior_np, tamaño_bloque, restriccion=False
+)
+
+# Imprimir los resultados
+print("Vectores de movimiento:", vectores_movimiento)
+print("Errores de predicción:", errores_prediccion)
+print("MSE promedio:", mse_promedio)
--- libs/uab/PRACTICA3/sessio3.py
+++ libs/uab/PRACTICA3/sessio3.py
@@ -1,62 +1,48 @@
-#from scipy import misc
+# from scipy import misc
 import cv2
 from scipy.fftpack import dct, idct
-from PIL import Image,ImageDraw  # necessari tenir instalar llibreria PILLOW
+from PIL import Image, ImageDraw  # necessari tenir instalar llibreria PILLOW
 import numpy as np
 import metrikz
 import time
+import matplotlib.pyplot as plt
 
-
-quantization_matrix =  [[16.,11.,10.,16.,24.,40.,51.,61.],
-                        [12.,12.,14.,19.,26.,58.,60.,55.],
-                        [14.,13.,16.,24.,40.,57.,69.,56.],
-                        [14.,17.,22.,29.,51.,87.,80.,62.],
-                        [18.,22.,37.,56.,68.,109.,103.,77.],
-                        [24.,35.,55.,64.,81.,104.,113.,92.],
-                        [49.,64.,78.,87.,103.,121.,120.,101.],
-                        [72.,92.,95.,98.,112.,100.,103.,99.]]
-
+quantization_matrix = [[16., 11., 10., 16., 24., 40., 51., 61.],
+                       [12., 12., 14., 19., 26., 58., 60., 55.],
+                       [14., 13., 16., 24., 40., 57., 69., 56.],
+                       [14., 17., 22., 29., 51., 87., 80., 62.],
+                       [18., 22., 37., 56., 68., 109., 103., 77.],
+                       [24., 35., 55., 64., 81., 104., 113., 92.],
+                       [49., 64., 78., 87., 103., 121., 120., 101.],
+                       [72., 92., 95., 98., 112., 100., 103., 99.]]
 
 
 def dct2(block):
-    return dct(dct(block, axis=0, norm = 'ortho'), axis=1, norm = 'ortho'); 
-                   
-
+    return dct(dct(block, axis=0, norm='ortho'), axis=1, norm='ortho');
 
 
 def idct2(block):
-    return idct(idct(block, axis=1, norm = 'ortho'), axis=0, norm = 'ortho');
-    
-    
-    
+    return idct(idct(block, axis=1, norm='ortho'), axis=0, norm='ortho');
+
 
 def quantization_process(block):
-    qm=np.zeros((8, 8))
+    qm = np.zeros((8, 8))
     for i in range(int(len(block))):
         for j in range(int(len(block))):
-            qm[i][j] = np.round( float(block[i][j]) / quantization_matrix[i][j])
+            qm[i][j] = np.round(float(block[i][j]) / quantization_matrix[i][j])
     return qm
-    
-    
+
 
 # Aplicar DCT y llamar la funcion de cuantizacion pel vector dels errors.
 def func_quantized(block):
-    qm=np.zeros((8, 8),dtype=int)  
-    qm[:,:] = quantization_process(dct2(block[:,:]))
+    qm = np.zeros((8, 8), dtype=int)
+    qm[:, :] = quantization_process(dct2(block[:, :]))
     return qm
-
-
-# El inverso de la operacion anterior
-def func_inverse(block):
-    inverse = inverse_process(block)
-    inverse = round(idct2(block_struct.data * quantization_matrix));
-
 
 
 # Funció per crear l'algoritme de motion compensation. DECODIFICADOR
 def func_motion_compensation(actual_position, motion_vector, errors_predicio):
-
-    ## 
+    ##
     ## Implementar motion compensation.
     ## Crear frame3 a partir de frame1 i MV
     ## i despres crear frame4 i corregir amb errors de prediccio
@@ -67,42 +53,36 @@ def func_motion_compensation(actual_position, motion_vector, errors_predicio):
     return 0
 
 
-
-
-
 if __name__ == '__main__':
-
-    #frame anterior
+    # frame anterior
     I1 = Image.open("frame0_1.png")
-    #frame actual
+    # frame actual
     I2 = Image.open("frame0_2.png")
 
-#   I1.show()
+    #   I1.show()
     img1 = I1.convert('L')
     img1.save('frame1_gray.png')
     img2 = I2.convert('L')
     img2.save('frame2_gray.png')
-#   img1.show()
-   
+    #   img1.show()
+
     frame1 = cv2.imread("frame1_gray.png")
     frame2 = cv2.imread("frame2_gray.png")
-    cv2.imshow('image',frame1)
-    frame1=frame1[:,:,0]
-    frame2=frame2[:,:,0]
-  
-    print("dimensions de la imatge= "  )
+    cv2.imshow('image', frame1)
+    frame1 = frame1[:, :, 0]
+    frame2 = frame2[:, :, 0]
+
+    print("dimensions de la imatge= ")
     print(frame1.shape)
-    dim=frame1.shape
-    
-    #vectors finals
-    actual_position=[]
-    motion_vector=[]
-    errors_prediction=[]
-       
+    dim = frame1.shape
+
+    # vectors finals
+    actual_position = []
+    motion_vector = []
+    errors_prediction = []
+
     # matriu per generar els blocs 
-    bl=np.zeros((8, 8))
-
-
+    bl = np.zeros((8, 8))
 
     #
     # GERENAR AQUI EL CODI PER FER EL MOTION VECTORS
@@ -116,54 +96,47 @@ if __name__ == '__main__':
     # GENERAR AQUI EL CODI DE VISUALITZACIO
     # crear una línia entre cada posició dels elements dels vectors actual_position i motion_vector que siguin diferents. ex. línia origen (16,32) a (8,32)
     # podeu fer servir  ImageDraw.Draw(img) i ImageDraw.line
-    
-    
-    
-    
+
     #############################
     ###
     ### DECODIFICADOR 
     ###
     #############################
-    
-    frame3 = np.zeros(frame1.shape,dtype=np.int16)
-    frame4 = np.zeros(frame1.shape,dtype=np.int16)
-    
+
+    frame3 = np.zeros(frame1.shape, dtype=np.int16)
+    frame4 = np.zeros(frame1.shape, dtype=np.int16)
+
     # crida a funcio metode motion compensation
     func_motion_compensation(actual_position, motion_vector, errors_prediction)
 
-    print ("ssim frame3,frame2:", metrikz.ssim(frame3,frame2))
-    print ("ssim frame4,frame2:", metrikz.ssim(frame4,frame2))
-
+    print("ssim frame3,frame2:", metrikz.ssim(frame3, frame2))
+    print("ssim frame4,frame2:", metrikz.ssim(frame4, frame2))
 
     ## Per mostrar per pantalla les quatre imatges.
-    plt.figure(1,figsize=(16,10))
+    plt.figure(1, figsize=(16, 10))
     plt.rcParams['image.cmap'] = 'gray'
     plt.subplot(221)
     plt.title("I1-frame anterior")
-    plt.imshow(frame1,vmin=0,vmax=255)
+    plt.imshow(frame1, vmin=0, vmax=255)
     plt.subplot(222)
     plt.title("I2-frame actual")
-    plt.imshow(frame2,vmin=0,vmax=255)
+    plt.imshow(frame2, vmin=0, vmax=255)
     plt.subplot(223)
     plt.title("I3-frame motion compensation")
-    plt.imshow(frame3,vmin=0,vmax=255)
+    plt.imshow(frame3, vmin=0, vmax=255)
     plt.subplot(224)
     plt.title("I4-frame I3 + errors prediction")
-    plt.imshow(frame4,vmin=0,vmax=255)
+    plt.imshow(frame4, vmin=0, vmax=255)
 
-## Per mostrar el mapa de calor de les variacions, blau menys error, vermell maxim error
-    plt.figure(2,figsize=(14,14))
+    ## Per mostrar el mapa de calor de les variacions, blau menys error, vermell maxim error
+    plt.figure(2, figsize=(14, 14))
     plt.rcParams['image.cmap'] = 'jet'
     plt.subplot(211)
     plt.title("I3 - I2")
-    vm=np.max(np.absolute(frame3-frame2))
-    plt.imshow(np.absolute(frame3-frame2),vmin=0,vmax=vm)#np.max(np.absolute(frame3-frame2)))
+    vm = np.max(np.absolute(frame3 - frame2))
+    plt.imshow(np.absolute(frame3 - frame2), vmin=0, vmax=vm)  # np.max(np.absolute(frame3-frame2)))
     plt.subplot(212)
     plt.title("I4 - I2")
-    plt.imshow(np.absolute(frame4-frame2),vmin=0,vmax=vm)#np.max(np.absolute(frame3-frame2)))
+    plt.imshow(np.absolute(frame4 - frame2), vmin=0, vmax=vm)  # np.max(np.absolute(frame3-frame2)))
 
     plt.show()
-
-    
-    
