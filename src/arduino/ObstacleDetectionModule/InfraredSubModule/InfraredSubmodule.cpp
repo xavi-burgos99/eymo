7ca05ac96280a0c7ed9381ba76373d8dd898d98c
GabrielJuan349 <81719519+GabrielJuan349@users.noreply.github.com>
2024-04-24 13:10:21 +0200

    Upload Sensors SemiFinished with class

--- src/arduino/ObstacleDetectionModule/InfraredSubModule/InfraredSubmodule.cpp
+++ src/arduino/ObstacleDetectionModule/InfraredSubModule/InfraredSubmodule.cpp
@@ -1,14 +1,21 @@
-include "main.h"
+#include <Arduino.h>
 
-void readInfrared(int pin) {
-    // Read sensor value
-    int sensorValue = 0;
-    sensorValue = analogRead(analogInPin);
+int readInfrared(int pin) {
+    //? Read sensor value
+    uint16_t sensorValue_raw = 0;
+    float sensorValue = 0.0f;
+    sensorValue_raw = analogRead(pin);
 
-    // print the results to the Serial Monitor:
-    Serial.print("Infrared sensor value: ", sensorValue);
+    //* print the results to the Serial Monitor:
+    Serial.print("Infrared sensor value: ", sensorValue_raw);
 
-    // TODO: filter unstable values
+    //*  filter unstable values
+    if(sensorValue_raw < 0 ){
+        return -1;
+    }
+
+    // transform the sensor value to distance in cm
+    sensorValue = (6787.0f /(float) sensorValue_raw - 3.0f) - 4.0f;
 
     return sensorValue;
 }
