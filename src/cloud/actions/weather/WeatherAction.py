567394da745ec39e2a149b090cc0d1ebf4205092
Samya <impsamya@gmail.com>
2024-05-24 20:54:58 +0200

    weather action almost done

--- src/cloud/actions/weather/WeatherAction.py
+++ src/cloud/actions/weather/WeatherAction.py
@@ -11,10 +11,12 @@ from src.cloud.actions.BaseAction import BaseAction
 from src.cloud.external.WeatherApi import WeatherApi
 
 from abc import ABC
-
+from src.cloud.external.TextToSpeech import text_to_speech
 
 class WeatherAction(BaseAction, ABC):
     OPTION_PARAM_NAME = "option"
+    LAT_PARAM_NAME = "latitude"
+    LON_PARAM_NAME = "longitude"
 
     BASE_URL = "http://dataservice.accuweather.com"
     API_KEY = "cveAxjGo1fpAYcd64xglPwlCqhCgqkjo"
@@ -27,8 +29,21 @@ class WeatherAction(BaseAction, ABC):
     def handle(self, parameters: dict):
         print("WeatherAction")
         assert self.OPTION_PARAM_NAME in parameters.keys(), super().parameter_must_be_sent(self.OPTION_PARAM_NAME)
+        assert self.LAT_PARAM_NAME in parameters.keys(), super().parameter_must_be_sent(self.LAT_PARAM_NAME)
+        assert self.LON_PARAM_NAME in parameters.keys(), super().parameter_must_be_sent(self.LON_PARAM_NAME)
 
-        response = self.weather_api.get_forecast(self.LOCATION_KEY, parameters[self.OPTION_PARAM_NAME])
-        return super().response_json('weather', response)
-    
+        response = self.weather_api.get_forecast(parameters[self.OPTION_PARAM_NAME], parameters[self.LAT_PARAM_NAME], parameters[self.LON_PARAM_NAME])
+        print(response.get("Day").get("IconPhrase"))
+        prompt = f"En español, dame una respuesta que contenga: Description: {response.get("Day").get("IconPhrase")} \
+            The temperature will be bettween {response.get("Temperature").get("Minimum").get("Value")} \
+            and {response.get("Temperature").get("Maximum").get("Value")} degrees"  
+        ##### TODO: return base64 audio with the weather forecast using gemini
+        model = GenerativeModel("gemini-pro")
+        result = model.generate_content(prompt)
+        print(result)
+        
+        
+        result_base64 = text_to_speech(result)
+        return super().response_json('weather', result_base64)
 
+        
--- src/cloud/external/WeatherApi.py
+++ src/cloud/external/WeatherApi.py
@@ -16,11 +16,24 @@ class WeatherApi(BaseApi):
         response.raise_for_status()  # Raise an exception for non-2xx responses
         return response.json()'''
 
-    def get_forecast(self, location_key: str, option:str ,days: int = 5) -> Dict[str, Any]:
+    def get_geoposition(self, latitude: str, longitude: str) -> Dict[str, Any]:
+        """
+        Retrieves the location key for a given latitude and longitude.
+        """
+        
+        endpoint = f"/locations/v1/cities/geoposition/search?apikey={self.api_key}&q={latitude}%2C{longitude}"
+        response = self.send_request("GET", endpoint)
+        response.raise_for_status()
+        return response.json().get("Key")
+
+
+    def get_forecast(self, option:str, latitude:str, longitude:str, days: int = 5) -> Dict[str, Any]:
         """
         Retrieves the weather forecast for a given location key for the specified number of days.
         """
-        number = 0 if option == "current" else 1
+        
+        location_key = self.get_geoposition(latitude, longitude)
+        number = 0 if option == "today" else 1
         endpoint = f"/forecasts/v1/daily/{days}day/{location_key}?apikey={self.api_key}"
         response = self.send_request("GET", endpoint)
         response.raise_for_status()  # Raise an exception for non-2xx responses
